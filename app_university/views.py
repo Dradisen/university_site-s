cd8f06f5ef603deada99aa39ad876224ae9453cc
Gleb Uvarov <ugs1996@mail.ru>
2020-08-04 00:35:33 +0300

    added queryset for order list

--- app_university/views.py
+++ app_university/views.py
@@ -5,9 +5,22 @@ from app_university.models import *
 
 
 class RectorateAPIView(generics.ListAPIView):
-    queryset = RectoratePosition.objects.all() 
+    #queryset = RectoratePosition.objects.all() 
     serializer_class = RectorateSerializer
+    fields = ['id', 'position_title', 'faculty', 'belong_id']
 
+    def get_queryset(self):
+        order_values = self.request.GET.getlist('order')
+        
+        if(len(order_values)):
+            for i, item in enumerate(order_values):
+                print(item[1:])
+                if not ((item[0] == "-" and item[1:] in self.fields) or (item in self.fields)):
+                    del order_values[i]
+                    
+            return RectoratePosition.objects.all().order_by(*order_values)
+
+        return RectoratePosition.objects.all()
 
 class FacultyAPIView(generics.ListAPIView):
     queryset = Faculty.objects.all() 
